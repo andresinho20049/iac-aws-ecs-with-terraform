f4343929d165b7fa62c08e862f904ec6372ad9c9
André Carlos <33970381+andresinho20049@users.noreply.github.com>
2024-07-16 20:26:26 -0300

    Update variables.tf
    add variables included on ecs.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,19 @@ variable "enviroment" {
 variable "region" {
   type = string
 }
+
+variable "service_name" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
+
+variable "container_port" {
+  type = number
+}
